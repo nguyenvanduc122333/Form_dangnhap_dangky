1aafc5efb29098e8161c1c5d06edbc3a65934c0a
nguyenvanduc122333 <nguyenvanduc03042002@gmail.com>
2023-12-31 00:04:55 +0700

    init project

---
+++ DAL/DatabaseAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    // tạo chuỗi kết nối CSDL
+    public static SqlConnection Connect()
+    {
+        string strcon = @"Data Source=DUC;Initial Catalog=Formdangnhap_demo;Integrated Security=True";
+        SqlConnection con = new SqlConnection(strcon);// khởi tạo connect
+        return con;
+    }
+    public class DatabaseAccess { 
+ 
+    }
+}
---
+++ DTO/TaiKhoan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public internal class TaiKhoan
+    {
+        public string sMaTk ( get; set; )
+        public string sTaiKhoan(get; set; )
+        public string sMatKhau(get; set; )
+        public int FK_iMaQuyen(get; set; )
+    }
+}
